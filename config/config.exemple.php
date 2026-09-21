c7b6d6cd6e895b65d671685fdce31d065ad9520c
Vincent <contact@vincentlopez.fr>
2026-09-21 11:36:06 +0200

    Configure local database settings

---
+++ config/config.exemple.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'dbname' => 'tomtroc',
+        'user' => '',
+        'password' => '',
+        'charset' => 'utf8mb4',
+    ],
+];
